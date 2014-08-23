441857afd7e5fdec1e5eabde6360de60d795f652
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-08-23 10:29:03 +0100

    Add devel packages and dashboard repo to cranberry

--- manifests/nodes/cranberry.pp
+++ manifests/nodes/cranberry.pp
@@ -5,4 +5,12 @@ node cranberry {
 	include base::common
 	include apache::server
 	include apache::passenger
+	include packages::devel
+
+	vcsrepo {
+		'/opt/puppet-dashboard':
+			ensure   => latest,
+			provider => git,
+			source   => 'https://github.com/sodabrew/puppet-dashboard.git';
+	}
 }
