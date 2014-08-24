1d83497a305c07abe01f0f9b4961575dd9c5bc78
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-08-24 22:59:19 +0100

    Service defines for pineberry

--- manifests/nodes/pineberry.pp
+++ manifests/nodes/pineberry.pp
@@ -8,4 +8,18 @@ node pineberry {
 	remotefile {
 		'/etc/httpd/conf.d/50-gitlab.conf': ;
 	}
+
+	service {
+		'redis':
+			ensure => true,
+			enable => true;
+
+		'gitlab-unicorn':
+			ensure => true,
+			enable => true;
+
+		'gitlab-sidekiq':
+			ensure => true,
+			enable => true;
+	}
 }
